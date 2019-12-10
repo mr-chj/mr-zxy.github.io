76cf457400106c66c11c9ef6a753faf255148789
mr-zxy <1989429861@qq.com>
2019-12-10 10:37:32 +0800

    zhao

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
+<<<<<<< HEAD
     <h1>This is an about page</h1>
     <p>赵旭阳改动了</p>
+=======
+    <h1>成浩杰</h1>
+>>>>>>> 48480aa566a18696c2330c8f5d5417eec4f5e1b6
   </div>
 </template>
