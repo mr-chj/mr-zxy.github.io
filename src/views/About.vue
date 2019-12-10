eb30dcc9e65a04d2e9162436150a0bde83921607
mr-zxy <1989429861@qq.com>
2019-12-10 10:22:42 +0800

    zhao

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>赵旭阳改动了</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <p>赵旭阳</p>
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    我第二次修改了
   </div>
 </template>
 
